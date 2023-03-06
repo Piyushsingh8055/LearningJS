32fa4e4d3ea842218db59604e6de576ea66e94b9
PIYUSH <bosspiyush98@gmail.com>
2023-03-06 17:00:45 +0530

    complted

--- array.js
+++ array.js
@@ -6,3 +6,24 @@ let n =arr.filter((x)=>{
     return x%10==0
 })
 console.log(n)
+
+
+
+
+// Array of sqauare of given number
+
+
+let num= arr.map((y)=>{
+    return y*y
+})
+console.log(num);
+
+
+
+// factorial of a given number from an array of first n natural numbers
+
+let arr1=[1,2,3,4,5]
+let number=arr1.reduce((x1,x2)=>{
+return x1*x2
+})
+console.log(number)
